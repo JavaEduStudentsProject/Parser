e4c52f1b4172e12dfbd6e4aec5931334c6c73076
elizavetakabak <kabakelizaveta@gmail.com>
2022-08-23 14:33:08 +0300

    ParserCSV with properties. FileWriter

--- src/main/java/parser/ParserCSV.java
+++ src/main/java/parser/ParserCSV.java
@@ -4,6 +4,7 @@ import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
@@ -30,26 +31,57 @@ public class ParserCSV extends Parser {
            for (int i = 1; i < r.size(); i++) {
                result.add(csvToJson(header, r.get(i)));
            }
-       } catch (IOException e) {
-           throw new RuntimeException(e);
-       } catch (CsvException e) {
+       } catch (IOException | CsvException e) {
            throw new RuntimeException(e);
        }
-
-       if(result.size() > 0) {
+        if(result.size() > 0) {
            String oldLastString = result.get(result.size()-1);
            String newLastString = oldLastString.substring(0,oldLastString.length()-2);
            result.remove(result.size()-1);
            result.add(newLastString);
        }
+        saveToJson(result);
         return result;
     }
 
     private String csvToJson(String[] header, String[] values) {
-        String result = "{\n";
+        String tmpH = null;
+        String tmpV = null;
+        String tmpH2 = null;
+        String tmpV2 = null;
         for (int i = 0; i < header.length; i++) {
+            if(header[i].equals("name")) {
+                tmpH = header[0];
+                header[0] = header[i];
+                header[i] = tmpH;
+                tmpV = values[0];
+                values[0] = values[i];
+                values[i] = tmpV;
+            } else if
+            (header[i].equals("price")) {
+                tmpH2 = header[1];
+                header[1] = header[i];
+                header[i] = tmpH2;
+                tmpV2 = values[1];
+                values[1] = values[i];
+                values[i] = tmpV2;
+            }
+        }
+        String result = "{\n";
+        String props = "\"" + "properties" + "\"" + " : " + "[" + "\n";
+        String result2 = "{\n";
+        for (int i = 0; i < 2; i++) {
             result += "\"" + header[i] + "\"" + " : " + "\"" + values[i] + "\"" + ",\n";
         }
+        for (int i = 2; i < header.length; i++) {
+            if (header[i] == header[header.length-1]) {
+                result2 += "\"" + header[i] + "\"" + " : " + "\"" + values[i] + "\"" + "\n";
+            } else {
+                result2 += "\"" + header[i] + "\"" + " : " + "\"" + values[i] + "\"" + ",\n";
+            }
+        }
+        result2+= "}]\n";
+        result +=props + result2;
         result+= "},\n";
         return result;
     }
@@ -60,5 +92,13 @@ public class ParserCSV extends Parser {
                 header[i] = headers.get(header[i]);
         }
         return header;
+    }
+
+    public void saveToJson(List<String> list) throws IOException {
+        FileWriter writer = new FileWriter("/Users/elizavetakabak/repos/ServiceDemoProject/mydata.json");
+        for (int i = 0; i < list.size(); i++) {
+            writer.write(list.get(i));
+        }
+        writer.close();
     }
 }
